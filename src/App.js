1c55ae4819dc69f4da459e898e1106eded6e9ec1
anushankar <anushankar@qburst.com>
2021-07-10 11:20:29 +0530

    Completed Routing changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { useEffect } from 'react';
 import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Detail from './pages/Detail/Detail';
+import Landing from './pages/Landing/Landing';
 
 function App() {
 
   useEffect(() => {
-  },[])
-  
+  }, [])
+
   return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/detail' component={Detail} />
+        <Route path='/' component={Landing} />
+      </Switch>
+    </Router>
+
   );
 }
 
